9e6d78877ef972ab829a1f847412e6a36cb3f60a
58160266 <58160266@go.buu.ac.th>
2017-10-23 23:24:51 +0700

    finish mock v.1

--- BookLookup.spec.js
+++ BookLookup.spec.js
@@ -1,9 +1,17 @@
 function BookLookup(AmazonService){
   this.service = AmazonService
-  this.echo = (isbn) =>{
-    var n = 'xxx'
-    var isbn = this.service(isbn)
-    return `${isbn}`
+  // this.echo = (isbn) =>{
+  //   var n = 'xxx'
+  //   var isbn = this.service(isbn)
+  //   return `${isbn}`
+  // }
+  this.search = (isbn) => {
+    var obj = this.service(isbn)
+    return {
+      title : obj.bookName,
+      image : obj.cover,
+      isbn : obj.isbn
+    }
   }
 
 }
@@ -13,16 +21,16 @@ test('BookLookup Mock',()=>{
           .mockReturnValue({
             bookName : 'basic node',
             cover : ' : D ',
-            isbn : 'isbn555'
+            isbn : 'isbn5555'
           })
   var book = new BookLookup(AmazonServiceMock)
-  var isbn = 'isbn555'
+  var isbn = 'isbn5555'
   //var result = app.echo(isbn)
   var bookInfo = book.search(isbn)
   expect(AmazonServiceMock).toHaveBeenCalled()
   expect(AmazonServiceMock).toHaveBeenCalledWith(isbn)
   //expect(result).toBe('12345679')
-  expect(bookInfo.title).tobe('basic node')
+  expect(bookInfo.title).toBe('basic node')
   expect(bookInfo).toHaveProperty('isbn')
   expect(bookInfo.isbn).toHaveLength(8)
 
